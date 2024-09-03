2b305eb3d4b89887e51f9942a1992d3ebc2e2ab8
GrahLnn <grahlnn@skiff.com>
2024-09-03 18:33:44 +0800

    d

--- nonebot_plugin_ddcheck/__init__.py
+++ nonebot_plugin_ddcheck/__init__.py
@@ -94,6 +94,7 @@ driver = nonebot.get_driver()
 @driver.on_bot_connect
 async def _():
     bot = get_bot()
+    asyncio.run(get_upcoming_bili_live(5714273))
     asyncio.create_task(check_timers(bot, vtb_data, ytb_data, bind_data))
     asyncio.create_task(watch_tweets(bot, vtb_data, bind_data))
 
