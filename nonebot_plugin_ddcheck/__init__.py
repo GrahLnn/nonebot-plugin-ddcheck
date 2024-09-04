ce39fecdda5fd8e5184ef126a2cc7cab142172c8
GrahLnn <grahlnn@skiff.com>
2024-09-04 23:39:27 +0800

    d

--- nonebot_plugin_ddcheck/__init__.py
+++ nonebot_plugin_ddcheck/__init__.py
@@ -486,6 +486,7 @@ async def handle_whenlive(bot: Bot, matcher: Matcher, msg: Message = CommandArg(
 async def watch_tweets(bot, vtb_data, bind_data):
     while True:
         tweets = await get_tweets()
+        logger.info("tweets: ", tweets)
         for vtb in vtb_data:
             await send_tweets(bot, vtb["sub_group"], bind_data, tweets)
         await asyncio.sleep(120)
